4145691813913a036a1e202fd4db8128194d9843
Carl Hain <crhain@gmail.com>
2019-09-13 16:29:05 -0500

    Create a stub for SongList component that will show a list of songs and wire it up to App component

---
+++ src/components/SongList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { selectSong } from '../actions';
+
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+export default SongList;
