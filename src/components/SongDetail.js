e84dfbd7fb5cbec8ada8d32c1946661ccbcf0fa3
Carl Hain <crhain@gmail.com>
2019-09-14 11:32:21 -0500

    Create basic SongDetail component that prints SongDetail to screen

---
+++ src/components/SongDetail.js
@@ -0,0 +1,10 @@
+import react, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SongDetail extends Component {
+  render() {
+    return <div>SongDetail</div>;
+  }
+}
+
+export default SongDetail;
