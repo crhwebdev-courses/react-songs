24b9412abc7327817befbf61554ea44af255285b
Carl Hain <crhain@gmail.com>
2019-09-14 11:41:33 -0500

    Change formating on right column of App component

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <div className="column eight wide">
           <SongList />
         </div>
-        <div className="column four wide">
+        <div className="column eight wide">
           <SongDetail />
         </div>
       </div>
